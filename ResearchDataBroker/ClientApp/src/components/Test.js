074a65a96175fe7e62ffca8cb8b6efd289d57daa
estherw66 <116259825+estherw66@users.noreply.github.com>
2023-11-30 14:06:36 +0100

    Add project

---
+++ ResearchDataBroker/ClientApp/src/components/Test.js
@@ -0,0 +1,38 @@
+﻿import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const Test = () => {
+    const [message, setMessage] = useState("")
+
+    //useEffect(() => {
+    //    fetch(`api/test/`)
+    //        .then((res) => {
+    //            return res.json()
+    //        })
+    //        .then(data => {
+    //            console.log(data)
+    //        })
+    //}, [])
+
+    useEffect(() => {
+        axios.get('/api/test')
+            .then(response => {
+                setMessage(response.data['message']);
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return (
+        <main>
+            <h1>Testing the api</h1>
+            {
+                (message !== "") ? <h3>{message}</h3> : <h3>Loading...</h3>
+            }
+        </main>
+    )
+}
+
+export default Test
---
+++ ResearchDataBroker/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResearchDataBroker.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class TestController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			Dictionary<string, string> message = new Dictionary<string, string>
+			{
+				{ "message", "Test message" }
+			};
+
+			return Ok(message);
+        }
+	}
+}
+
