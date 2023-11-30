a272e8fef0f3eef9600d21d869d33116795e995d
estherw66 <116259825+estherw66@users.noreply.github.com>
2023-11-30 14:17:27 +0100

    Remove unnecessary files

--- ResearchDataBroker/ClientApp/src/AppRoutes.js
+++ ResearchDataBroker/ClientApp/src/AppRoutes.js
@@ -1,5 +1,3 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 import Test from "./components/Test";
 
@@ -9,17 +7,9 @@ const AppRoutes = [
     element: <Home />
   },
   {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-    },
-    {
-        path: '/test-api',
-        element: <Test />
-    }
+    path: '/test-api',
+    element: <Test />
+  }
 ];
 
 export default AppRoutes;
