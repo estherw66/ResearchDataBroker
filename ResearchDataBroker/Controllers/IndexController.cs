81e38ac7566778be2abbb55e121a326ee5968a41
estherw66 <116259825+estherw66@users.noreply.github.com>
2023-12-07 10:26:54 +0100

    Update IndexController.cs

--- ResearchDataBroker/Controllers/IndexController.cs
+++ ResearchDataBroker/Controllers/IndexController.cs
@@ -4,6 +4,19 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class IndexController : ControllerBase
 {
+    private readonly IIndexService _indexService;
+
+    public IndexController(IIndexService indexService)
+    {
+        _indexService = indexService;
+    }
     // index files
     // enter url of dataset
+
+    [HttpGet]
+    public async Task<IActionResult> GetFiles([FromBody]GetDatasetRequestDTO request)
+    {
+        // DataverseLatestVersionModel latestVersion = await _indexService
+        return Ok();
+    }
 }
